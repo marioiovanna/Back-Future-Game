0384fee42cd5bdb0ab2643fb0fa98412f0302334
marioiovanna <marioiovanna@hotmail.com>
2017-08-13 06:42:42 -0400

    add fight mode

--- assests/java/startGame.js
+++ assests/java/startGame.js
@@ -2,16 +2,30 @@ var players = [
     {
         name: 'biff',
         life: 300,
-        hitp: 30
+        hitp: 18
     },
     {
         name: 'doc',
-        life: 200,
+        life: 250,
         hitp: 10
+    },
+    {
+        name: 'martin',
+        life: 300,
+        hitp: 15
+    },
+    {
+        name: 'mcfly',
+        life: 220,
+        hitp: 5
     }];
 
 var playerchoose = [];
 var playervs = [];
+
+var selected = 0;
+
+var fightnow = false;
 
 function startgame() {
     // START GAME press button trigger this ...
@@ -19,43 +33,87 @@ function startgame() {
         $('.selecttext').fadeIn(3000);
         $('.bt1').fadeOut(400);
 
-        playerselector()
+        playerselector();
     })
 }
 
-function playerselector() {
-    // Selector player
-    $('.pside').on('click', this, function () {
+function fight() {
+    if (fightnow === true) {
+        $('.fight').on('click', function () {
 
-        if (players[0].name === this.value) {
-            playerchoose.push(players[0]);
-            $(this).appendTo('.select');
+            playerchoose[0].life -= playervs[0].hitp;
+            $('.lifeplayer').html('LIFE: ' + playerchoose[0].life);
+            playerchoose[0].hitp += Math.floor((Math.random() * 10) + 1);
 
-            $('.biff').appendTo('.select');
-            $('.biffbtn').attr('disabled', true);
-            $('.selecttext').hide();
+            playervs[0].life -= playerchoose[0].hitp;
+            $('.lifevs').html('LIFE: ' + playervs[0].life );
+            playervs[0].hitp += Math.floor((Math.random() * 10) + 1);
 
-            $('.choosevs').html('Choose VS').show();
+            console.log(playerchoose[0].hitp);
+            console.log(playervs[0].hitp)
 
-            // Select Vs
-            $('.pside').on('click', this, function () {
+            if (playervs[0].life <= -1) {
+                alert('aaaaaaaaaaaaaaa')
+            }
 
 
-                if (players[1].name === this.value) {
 
-                    playervs.push(players[1]);
+
+
+        })
+
+    }
+}
+
+function vsselector() {
+    if (selected === 1) {
+        // Select Vs
+        $('.pside').on('click', this, function () {
+
+            for (var i = 0; i < players.length; i++) {
+
+                if (players[i].name === this.value) {
+                    playervs.push(players[i]);
                     $(this).appendTo('.vs');
                     $('.choosevs').html('VS');
-                    $('.docbtn').attr('disabled', true);
+                    $('.pside').attr('disabled', true);
+                    $('.lifevs').html('LIFE: ' + playervs[0].life );
 
+                    $('.fight').fadeIn(3000);
 
+                    fightnow = true;
+                    fight();
+                }
+            }
+        })
+    }
 
+}
 
+function playerselector() {
+    if (selected === 0) {
+        // Select player
+        $('.pside').on('click', this, function () {
+
+            for (var i = 0; i < players.length; i++) {
+
+                if (players[i].name === this.value) {
+
+                    playerchoose.push(players[i]);
+                    $(this).appendTo('.select');
+                    $(this).attr('disabled', true);
+                    $('.selecttext').hide();
+                    $('.lifeplayer').html('LIFE: ' + playerchoose[0].life);
+
+                    $('.choosevs').html('Choose VS').show();
+                    selected++;
+
+                    vsselector()
                 }
 
-            })
-        }
-    })
+            }
+        })
+    }
 }
 
 $(document).ready(function () {
